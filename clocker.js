ffb8913978d82cbd545b071017e02ebbbe62db5c
Tymofiy Regenel <vt222_rtyu@student.ztu.edu.ua>
2023-03-07 18:55:19 +0200

    added start script to package.json

--- clocker.js
+++ clocker.js
@@ -1,6 +1,7 @@
 const { Telegraf } = require('telegraf')
 const { message } = require('telegraf/filters')
 const texts = require('./texts.js')
+const times = require('./times.js')
 
 module.exports = class Clocker {
     constructor(userdb, hwdb, tabledb, bot) {
@@ -12,8 +13,8 @@ module.exports = class Clocker {
         this.msInMinute = 60000
         this.msInDay = this.msInMinute * 1440
 
-        this.clock(this.daily, this.udb, this.tdb, this.hwdb, this.bot)
-        setInterval(this.clock, this.msInMinute, this.daily, this.udb, this.tdb, this.hwdb, this.bot)
+        this.clock(this.daily, this.udb, this.tdb, this.hwdb, this.bot, times)
+        setInterval(this.clock, this.msInMinute, this.daily, this.udb, this.tdb, this.hwdb, this.bot, times)
     }
 
     plan() {
@@ -21,28 +22,39 @@ module.exports = class Clocker {
         let nowMs = ((parseInt(now.getHours()) * 60 + parseInt(now.getMinutes())) * 60 + parseInt(now.getSeconds())) * 1000 + parseInt(now.getMilieconds())
         setTimeout(this.plan, this.msInDay - nowMs)
 
-        for(let [key, value] of Object.entries(this.udb.db)){
-            if(!value.settings.dailySend) continue
-            if(value.settings.dailyTime*this.msInMinute <= nowMs) continue
+        for (let [key, value] of Object.entries(this.udb.db)) {
+            if (!value.settings.dailySend) continue
+            if (value.settings.dailyTime * this.msInMinute <= nowMs) continue
             setTimeout(this.daily, value.settings.dailyTime * this.msInMinute - nowMs, key, this.tdb, this.hwdb, this.bot)
         }
     }
 
-    clock(daily, udb, tdb, hwdb, bot){
+    clock(daily, udb, tdb, hwdb, bot, times) {
         let now = new Date()
         let nowMin = ((parseInt(now.getHours()) * 60 + parseInt(now.getMinutes())))
         let nowMs = nowMin + parseInt(now.getSeconds()) * 1000 + parseInt(now.getMilliseconds())
-    
-        for(let [key, value] of Object.entries(udb.db)){
-            if(!value.settings.dailySend) continue
-            if(value.settings.dailyTime != nowMin) continue
+
+        for (let [key, value] of Object.entries(udb.db)) {
+            if (!value.settings.dailySend) continue
+            if (value.settings.dailyTime != nowMin) continue
             daily(key, tdb, hwdb, bot)
+        }
+
+        const day = tdb.today()
+        let lesson
+
+        for (let i = 0; i <= 6; i += 1) {
+            lesson = tdb.db[day].lessons[i]
+            if (lesson.type != "p") continue
+            if (nowMin == parseInt(times[i].start) + 45) {
+                hwdb.write(lesson.subject, "empty", "clocker")   
+            }
         }
 
         console.log("Clock")
     }
 
-    daily(id, tdb, hwdb, bot){
+    daily(id, tdb, hwdb, bot) {
         const day = tdb.soonest()
         const date = new Date(tdb.db[day].date)
         const dayName = tdb.db[day].name
@@ -77,6 +89,6 @@ module.exports = class Clocker {
             }
         }
 
-        bot.telegram.sendMessage(id, texts.dailyhw(dayName, dayDate, lessons, hws), {parse_mode: 'HTML'})
+        bot.telegram.sendMessage(id, texts.dailyhw(dayName, dayDate, lessons, hws), { parse_mode: 'HTML' })
     }
 }
